c77720d8c8985292c6005a2c3f5e795650bf9908
Mathias Rabe <mathias-rabe@post-x.de>
2012-03-27 20:12:14 +0200

    first commit

---
+++ weather_station/main.c
@@ -0,0 +1,13 @@
+//----------------------------------------------------------------------------
+// C main line
+//----------------------------------------------------------------------------
+
+#include <m8c.h>        // part specific constants and macros
+#include "PSoCAPI.h"    // PSoC API definitions for all User Modules
+
+
+void main(void)
+{
+	// M8C_EnableGInt ; // Uncomment this line to enable Global Interrupts
+	// Insert your main routine code here.
+}
