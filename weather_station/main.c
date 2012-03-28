0584d89b781a0dc64f9d19ceeb03cb86c471fdd3
Mathias Rabe <mathias-rabe@post-x.de>
2012-03-28 10:26:33 +0200

    Ports specificated

--- weather_station/main.c
+++ weather_station/main.c
@@ -1,10 +1,24 @@
 //----------------------------------------------------------------------------
 // C main line
+// Ports:
+// P0_0 = MCLK
+// P0_1 = XCLR adc reset
+// P0_2 = Button SW
+// P0_3 = windspeed input
+// P0_4 = rainsensor input
+// P0_5 = sunlight (solar) input
+// P0_6 = I2C SDA
+// P0_7 = I2C SCL
+// P1_0 = Gray Code
+// P1_1 = Gray Code
+// P1_1 = Gray Code
+// P2 = LCD
 //----------------------------------------------------------------------------
 
 #include <m8c.h>        // part specific constants and macros
 #include "PSoCAPI.h"    // PSoC API definitions for all User Modules
 
+// init
 
 void main(void)
 {
