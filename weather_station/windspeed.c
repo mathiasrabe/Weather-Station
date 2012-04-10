be4e0fbf0f1b4e625502c889ac1b3b93a3bfeeac
Mathias Rabe <mathias-rabe@post-x.de>
2012-04-10 11:20:56 +0200

    cleaned up code

--- weather_station/windspeed.c
+++ weather_station/windspeed.c
@@ -1,6 +1,3 @@
-#define PORT PRT0DR
-#define RAINPIN 0b00001000 // P0_3
-
 #include "PSoCAPI.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,7 +6,7 @@ void windsensor(char *firstLine, char *secondLine) {
 	int windSample = 0x00;
 
 	windSample = SAR6_wind_cGetSample();
-	windSample += 31; // make unsigned int
+	windSample += 30; // make unsigned int
 	windSample *= 37; // to get km/h
 	
 	csprintf(firstLine, "Windspeed & Dire");
